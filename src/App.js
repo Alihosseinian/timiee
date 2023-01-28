ca01c71700cf80dddce83bfd00f36debc9412bb8
Alihosseinian <Alihoseininan@gmail.com>
2023-01-28 16:17:10 +0330

    finally version login page

--- src/App.js
+++ src/App.js
@@ -29,7 +29,7 @@ function App() {
             <div class="password-container">
               <input
                 type="password"
-                placeholder="password"
+                placeholder="Password"
                 class="password"
                 required
               ></input>
