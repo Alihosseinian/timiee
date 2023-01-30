6abee2db4ead5cca3547d1158d41dcf71d0158a3
Alihosseinian <Alihoseininan@gmail.com>
2023-01-30 14:26:17 +0330

    build ok test for gh-page

--- src/App.js
+++ src/App.js
@@ -6,50 +6,50 @@ import eye_slashLogo from "./assets/eye-slash.svg";
 
 function App() {
   return (
-    <div class="container">
-      <div class="image-box"></div>
-      <div class="login-form">
-        <div class="question">
+    <div className="container">
+      <div className="image-box" />
+      <div className="login-form">
+        <div className="question">
           <p>
             Not a member?
             <span> </span>
             <span> </span>
-            <a class="register" href="#">
+            <a className="register" href="#">
               Resgister now
             </a>
           </p>
         </div>
-        <div class="form">
-          <div class="title">
+        <div className="form">
+          <div className="title">
             <h2>Hello Again!</h2>
             <p>wellcome back you've been missed!</p>
           </div>
-          <form class="input-box">
+          <form className="input-box">
             <input type="text" placeholder="Enter username" required></input>
-            <div class="password-container">
+            <div className="password-container">
               <input
                 type="password"
                 placeholder="Password"
-                class="password"
+                className="password"
                 required
               ></input>
-              <img src={eye_slashLogo} alt="eye_slashLogo" class="eye_slash" />
+              <img src={eye_slashLogo} alt="eye slashLogo" className="eye_slash" />
             </div>
-            <div class="recovery-password">
+            <div className="recovery-password">
               <a href="#">Recovery Password</a>
             </div>
-            <input type="submit" class="submit" value="Sign In"></input>
+            <input type="submit" className="submit" value="Sign In"></input>
           </form>
-          <div class="footer">
-            <p class="line">Or continue with</p>
-            <div class="icon">
-              <div class="gmail">
+          <div className="footer">
+            <p className="line">Or continue with</p>
+            <div className="icon">
+              <div className="gmail">
                 <img src={gmailLogo} alt="gmailLogo" />
               </div>
-              <div class="apple-id">
+              <div className="apple-id">
                 <img src={appleLogo} alt="appleLogo" />
               </div>
-              <div class="facebook">
+              <div className="facebook">
                 <img src={facebookLogo} alt="facebookLogo" />
               </div>
             </div>
